828c7121021bce0a332d4e0bc697fb54239db44c
Eugene Zhuravlev <zhuravlev.dev@gmail.com>
2018-07-25 21:44:51 +0300

    Изменил способ создания event popup.

--- src/js/view.js
+++ src/js/view.js
@@ -87,18 +87,101 @@ class View extends EventEmitter {
         this.emit('dateClick', targetDate);
     }
 
+    //Перенести в helpers
+    createElement(tag, attr, inner, ...child) {
+        let elem = document.createElement(tag);
+
+        if(inner) {
+            elem.innerHTML = inner;
+        };
+
+        for(let key in attr) {
+            elem.setAttribute(key, attr[key])
+        };
+
+        console.log(elem);
+
+        if(child) {
+            console.log(child);
+            for(let i = 0; i < child.length; i++) {
+                elem.appendChild(child[i])
+            };
+        }
+
+        return elem;
+    }
+
+    getEventElements() {
+        
+        let eventNameInp = this.createElement('input', {
+            type: 'text',
+            class: 'event-detail__name event-detail__inp completed',
+            id: 'event-detail-name'
+        });
+
+        let eventTimeInp = this.createElement('input', {
+            type: 'text',
+            class: 'event-detail__time event-detail__inp completed',
+            id: 'event-detail-time',
+            placeholder: 'Время'
+        });
+
+        let eventPartyInp = this.createElement('input', {
+            type: 'text',
+            class: 'event-detail__party event-detail__inp completed',
+            id: 'event-detail-party',
+            placeholder: 'Имена участников'
+        });
+
+        let eventDescInp = this.createElement('textarea', {
+            class: 'event-detail__desc',
+            id: 'event-detail-desc',
+            placeholder: 'Описание'
+        });
+
+        let eventComletedBtn = this.createElement('button', {
+            class: 'event-detail__completed event-detail__btn',
+            id: 'event-detail-completed'
+        }, 'Готово');
+
+        let eventDelBtn = this.createElement('button', {
+            class: 'event-detail__del event-detail__btn',
+            id: 'event-detail-del'
+        }, 'Удалить');
+
+        let container = this.createElement('div', {
+            id: 'event-detail',
+            class: 'event-detail'
+        }, '', eventNameInp, eventTimeInp, eventPartyInp, eventDescInp, eventComletedBtn, eventDelBtn);
+
+        return container;
+    }
+
     showEventDetail(event) {
-        this.eventDetailName.value = event.name;
-        this.eventDetailTime.value = event.time ? event.time : '';
-        this.eventDetailParty.value = event.party ? event.party : '';
-        this.eventDetailDesc.value = event.desc ? event.desc : '';
 
-        let dateItem = document.querySelector(`.calendar td[data-date='${event.date}']`);
+        // `<div class="event-detail" id="event-detail">
+        //     <input type="text" class="event-detail__name event-detail__inp completed" value="Праздник труда !" placeholder="" id="event-detail-name">
+        //     <input type="text" class="event-detail__time event-detail__inp completed" placeholder="Время" id="event-detail-time">
+        //     <input type="text" class="event-detail__party event-detail__inp" value="" placeholder="Имена участников" id="event-detail-party">
+        //     <textarea class="event-detail__desc" placeholder="Описание" id="event-detail-message"></textarea>
+        //     <button class="event-detail__completed event-detail__btn" id="event-detail-completed">Готово</button>
+        //     <button class="event-detail__del event-detail__btn" id="event-detail-del">Удалить</button>
+        // </div>`
 
-        dateItem.appendChild(this.eventDetailPopup);
+        // this.eventDetailName.value = event.name;
+        // this.eventDetailTime.value = event.time ? event.time : '';
+        // this.eventDetailParty.value = event.party ? event.party : '';
+        // this.eventDetailDesc.value = event.desc ? event.desc : '';
 
-        console.log(event);
-        this.eventDetailPopup.classList.add('active');
+        // let dateItem = document.querySelector(`.calendar td[data-date='${event.date}']`);
+
+        // dateItem.appendChild(this.eventDetailPopup);
+
+        // this.eventDetailPopup.classList.add('active');
+
+        const elem = this.getEventElements();
+
+        console.log(elem);
         
     }
 
